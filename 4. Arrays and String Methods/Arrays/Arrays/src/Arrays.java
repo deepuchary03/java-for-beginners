992b4772fb085e3777093c3d15d0e07155e452d7
S Pranav <129598591+deepuchary03@users.noreply.github.com>
2025-04-16 22:20:47 +0530

    Update Arrays.java

--- 4. Arrays and String Methods/Arrays/Arrays/src/Arrays.java
+++ 4. Arrays and String Methods/Arrays/Arrays/src/Arrays.java
@@ -2,7 +2,6 @@ public class Arrays
 {
    public static void main(String[] args)
    {
-      //week days
       String[] weekDays = new String[7];
       weekDays[0] = "Monday ";
       weekDays[1] = "Tuesday ";
@@ -11,11 +10,8 @@ public class Arrays
       weekDays[4] = "Friday ";
       weekDays[5] = "Saturday ";
       weekDays[6] = "Sunday ";
-
-      //prime numbers
       int[] primes = {2, 3, 5, 7, 11, 13, 17};
 
-      //the way to print an array in Java is to loop through it
       for (int i=0; i<7; i++)
       {
          System.out.print(weekDays[i]);
